8b4e4f10ef1c9078b15f70a45d572394142d028a
Frederico Minuzzi <freddyminu@gmail.com>
2023-01-27 11:44:31 -0300

    fix: changed test format, removed 1 test, added 5

--- src/lib/__test__/data.logic.test.ts
+++ src/lib/__test__/data.logic.test.ts
@@ -1,9 +1,47 @@
 import { parseTagoObject } from "../data.logic";
-import { obj, objResult } from "./mocks/data.logic.mock";
+import { obj } from "./mocks/data.logic.mock";
 
 describe("parseTagoObject", () => {
-  test("parseTagoObject", () => {
-    expect(parseTagoObject(obj, "1588160000000")).toEqual(objResult);
+  const result = parseTagoObject(obj, "1588160000000");
+  // testing variables
+  test("parseTagoObject variables", () => {
+    expect(result[0].variable).toBe("name");
+    expect(result[1].variable).toBe("user_amount");
+    expect(result[2].variable).toBe("active");
+    expect(result[3].variable).toBe("device_amount");
+    expect(result[4].variable).toBe("location");
+  });
+  // testing values
+  test("parseTagoObject values", () => {
+    expect(result[0].value).toBe("stringValuee");
+    expect(result[1].value).toBe(2);
+    expect(result[2].value).toBe(true);
+    expect(result[3].value).toBe(4);
+    expect(result[4].value).toBe(5);
+  });
+  // testing group
+  test("parseTagoObject group", () => {
+    expect(result[0].group).toBe("1588160000000");
+    expect(result[1].group).toBe("1588160000000");
+    expect(result[2].group).toBe("1588160000000");
+    expect(result[3].group).toBe("1588160000000");
+    expect(result[4].group).toBe("1588160000000");
+  });
+  // testing time
+  test("parseTagoObject time", () => {
+    expect(result[0].time).toBe(null);
+    expect(result[1].time).toBe(null);
+    expect(result[2].time).toBe(null);
+    expect(result[3].time).toBe("2020-01-01T00:00:00.000Z");
+    expect(result[4].time).toBe("2020-01-01T00:00:00.000Z");
+  });
+  // testing metadata
+  test("parseTagoObject metadata", () => {
+    expect(result[0].metadata).toBe(null);
+    expect(result[1].metadata).toBe(null);
+    expect(result[2].metadata).toEqual({ color: "Green" });
+    expect(result[3].metadata).toBe(null);
+    expect(result[4].metadata).toBe(null);
   });
 
   test("parseTagoObject with empty body", () => {

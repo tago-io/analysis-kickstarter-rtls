349823819013628c9382ef751c798a989c02133b
Frederico Minuzzi <freddyminu@gmail.com>
2023-02-20 19:10:14 -0300

    feat: added zod to devide edit

--- src/services/device/edit.ts
+++ src/services/device/edit.ts
@@ -1,20 +1,21 @@
 import { Utils } from "@tago-io/sdk";
-import { Data } from "@tago-io/sdk/out/common/common.types";
 import getDevice from "../../lib/getDevice";
+import validation from "../../lib/validation";
 import { ServiceParams } from "../../types";
-
-function getFormVariables(scope: Data[]) {
-  const dev_id = scope[0].device;
-  const dev_name = scope.find((x) => x.variable === "dev_name");
-  const new_site_id_data = scope.find((x) => x.variable === "dev_site");
-
-  return { dev_name, new_site_id_data, dev_id };
-}
+import { getDeviceVariables } from "./model/edit.model";
 
 async function editSensor({ config_dev, scope, account }: ServiceParams) {
+  const dev_id = scope[0].device;
   // fetching info
-  const { dev_name, new_site_id_data, dev_id } = getFormVariables(scope);
+  const validate = validation("org_validation", config_dev);
+  const { dev_name, new_site_id_data } = await getDeviceVariables(scope, validate);
   const org_dev = await Utils.getDevice(account, dev_id);
+
+  // get device tags
+  const dev_tags = (await account.devices.info(dev_id)).tags;
+  console.log("site_dev_tags", dev_tags);
+  // get tag named device_id
+  const dev_id_tag = dev_tags.find((tag) => tag.key === "device_id");
 
   // getting site device
   let { tags } = await account.devices.info(dev_id);
@@ -27,47 +28,40 @@ async function editSensor({ config_dev, scope, account }: ServiceParams) {
 
   if (dev_name) {
     // deleting prev data in settings_device
-    await config_dev.deleteData({ groups: dev_data.id, variables: "dev_name" });
-    await org_dev.deleteData({ groups: dev_data.id, variables: "dev_name" });
-    await site_dev.deleteData({ groups: dev_data.id, variables: "dev_name" });
+    await config_dev.deleteData({ groups: dev_id_tag.value, variables: "dev_name" });
+    await org_dev.deleteData({ groups: dev_id_tag.value, variables: "dev_name" });
+    await site_dev.deleteData({ groups: dev_id_tag.value, variables: "dev_name" });
 
     // sending to settings new info
-    await config_dev.sendData({ ...dev_data, metadata: { ...dev_data.metadata, label: dev_name.value as string }, time: null });
-    await org_dev.sendData({ ...dev_data, metadata: { ...dev_data.metadata, label: dev_name.value as string }, time: null });
-    await site_dev.sendData({ ...dev_name, metadata: { ...dev_data.metadata, label: dev_name.value as string }, time: null });
+    await config_dev.sendData({ ...dev_data, metadata: { ...dev_data.metadata, label: dev_name }, time: null });
+    await org_dev.sendData({ ...dev_data, metadata: { ...dev_data.metadata, label: dev_name }, time: null });
+    await site_dev.sendData({ ...(dev_name as any), metadata: { ...dev_data.metadata, label: dev_name }, time: null });
 
     // updating device name
-    await account.devices.edit(dev_id, { name: dev_name.value as string });
+    await account.devices.edit(dev_id, { name: dev_name });
 
     // editing bucket name
     const bucket_id = (await account.devices.info(dev_id)).bucket.id;
-    await account.buckets.edit(bucket_id, { name: dev_name.value as string });
+    await account.buckets.edit(bucket_id, { name: dev_name });
 
     // updating asset list
     await org_dev.deleteData({ variables: "asset_list", groups: dev_id });
-    await org_dev.sendData({ ...dev_name, group: dev_id });
+    await org_dev.sendData({ ...(dev_name as any), group: dev_id });
   }
 
   if (new_site_id_data) {
-    const new_site_dev = await getDevice(account, new_site_id_data.value as string);
-    // deleting prev info
-    // await config_dev.deleteData({ serie: dev_data.id });
-    // await org_dev.deleteData({ serie: dev_data.id });
-    await site_dev.deleteData({ groups: dev_data.id }); //prev site
-
-    // updating data
-    // await config_dev.sendData({ ...dev_data_site, value: new_site_id_data.metadata.label as string });
-    // await org_dev.sendData({ ...dev_data_site, value: new_site_id_data.metadata.label as string });
-    // await site_dev.sendData({ ...dev_data_site, value: new_site_id_data.metadata.label as string });
-    await new_site_dev.sendData(dev_data_site);
+    console.log("new_site_id_data", new_site_id_data);
+    const site_dev = await getDevice(account, new_site_id_data);
+    await site_dev.deleteData({ groups: dev_id_tag.value });
+    await site_dev.sendData(dev_data_site);
 
     // updating tags array
     tags = tags.filter((x) => !["site_id"].includes(x.key));
-    tags.push({ key: "site_id", value: new_site_id_data.value as string });
+    tags.push({ key: "site_id", value: new_site_id_data });
 
     await account.devices.edit(dev_id, { tags });
   }
   return;
 }
 
-export { getFormVariables, editSensor };
+export { editSensor };
